1052f5939fec3773de242f8c7036f515521bbd4e
yarikkot04 <yarikkotenkoim13@gmail.com>
2023-03-18 22:49:53 +0200

    added a function to resolve a postfix expression

--- implementation.go
+++ implementation.go
@@ -2,8 +2,59 @@ package lab2
 
 import "fmt"
 
-// TODO: document this function.
-// PrefixToPostfix converts
-func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+func SolvePostfixExpression(expression string) (string, error) {
+	stack := make([]float32, 0)
+	for i := 0; i < len(expression); i++ {
+		switch string(expression[i]) {
+		case "+", "-", "*", "/", "^":
+			if len(stack) < 2 {
+				return "", errors.New("invalid expression")
+			}
+		}
+		switch string(expression[i]) {
+		case " ":
+			continue
+		case "+":
+			num2 := stack[len(stack)-1]
+			num1 := stack[len(stack)-2]
+			stack = stack[:len(stack)-2]
+			result := num1 + num2
+			stack = append(stack, result)
+		case "*":
+			num2 := stack[len(stack)-1]
+			num1 := stack[len(stack)-2]
+			stack = stack[:len(stack)-2]
+			result := num1 * num2
+			stack = append(stack, result)
+		case "-":
+			num2 := stack[len(stack)-1]
+			num1 := stack[len(stack)-2]
+			stack = stack[:len(stack)-2]
+			result := num1 - num2
+			stack = append(stack, result)
+		case "/":
+			num2 := stack[len(stack)-1]
+			num1 := stack[len(stack)-2]
+			stack = stack[:len(stack)-2]
+			result := num1 / num2
+			stack = append(stack, result)
+		case "^":
+			num2 := stack[len(stack)-1]
+			num1 := stack[len(stack)-2]
+			stack = stack[:len(stack)-2]
+			result := math.Pow(float64(num1), float64(num2))
+			stack = append(stack, float32(result))
+		default:
+			num, err := strconv.ParseFloat(string(expression[i]), 32)
+			if err != nil {
+				return "", errors.New("invalid expression")
+			}
+			stack = append(stack, float32(num))
+		}
+	}
+	if len(stack) != 1 {
+		return "", errors.New("invalid expression")
+	}
+	strRes := fmt.Sprintf("%.2f", stack[0])
+	return strRes, nil
 }
